73d1086ca502f569d369a95eb7f30725e18f2723
Ludovico Russo <ludus.russo@gmail.com>
2016-11-30 15:51:28 +0100

    first commit

--- dotbot_ros.py
+++ dotbot_ros.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 
-from rospy import Rate, Publisher, Subscriber
+from rospy import Rate, Publisher, Subscriber, init_node
 
 class DotbotNode:
     main_rate = None
-
+    node_name = 'dotbot_node'
     def __init__(self):
         self.setup()
+        init_node(self.node_name)
         if self.main_rate is not None:
             while not rospy.is_shutdown():
                 self.loop()
